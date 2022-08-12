2e8caafb7c6fbf6806bc5221d019076c64fde27d
antonino2306 <antonino2306@gmail.com>
2022-08-12 15:57:09 +0200

    Modificato spostamento file
    Aggiunta funzione che controlla il nome del file e lo sposta solo se non è già presente un file con lo stesso nome

--- auto.py
+++ auto.py
@@ -2,7 +2,8 @@ from os import scandir, path
 import sys
 import time
 import logging
-import shutil
+from shutil import move
+from tracemalloc import start
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
@@ -11,6 +12,19 @@ image_dir = "/home/antonino/Scaricati/Immagini"
 video_dir = "/home/antonino/Scaricati/Video"
 audio_dir = "/home/antonino/Scaricati/Audio"
 docs_dir = "/home/antonino/Scaricati/Docs"
+
+
+def move_file(start_dir, end_dir, file_name):
+    files_list = []
+    with scandir(end_dir) as destination:
+        for element in destination:
+            if element.is_file():
+                files_list.append(element.name)
+    
+    if file_name in files_list:
+        print("Error")
+    else:
+        move(start_dir, end_dir)
 
 
     
@@ -23,19 +37,19 @@ class MoveHandler(FileSystemEventHandler):
 
                 if name.endswith(".jpg") or name.endswith(".jpeg") or name.endswith(".png"):
                     destination_dir = image_dir
-                    shutil.move(element, destination_dir)
+                    move_file(element, destination_dir, name)
 
                 elif name.endswith(".docx") or name.endswith(".pdf") or name.endswith(".xlsx"):
                     destination_dir = docs_dir
-                    shutil.move(element, destination_dir)
+                    move_file(element, destination_dir, name)
                 
                 elif name.endswith(".mp3") or name.endswith(".waw"):
                     destination_dir = audio_dir
-                    shutil.move(element, destination_dir)
+                    move_file(element, destination_dir, name)
                 
                 elif name.endswith(".mp4") or name.endswith(".avi") or name.endswith(".webm"):
                     destination_dir = video_dir
-                    shutil.move(element, destination_dir)
+                    move_file(element, destination_dir, name)
 
 
 
